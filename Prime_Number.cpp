00019597e7ebc591319b3c2c2151e123ec46006a
Kusuma.B.J <kusumabj040406@gmail.com>
2026-02-27 22:04:29 +0530

    Add files via upload

---
+++ Prime_Number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int num;
+    bool isPrime = true;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num <= 1)
+        isPrime = false;
+
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+        cout << "Prime Number";
+    else
+        cout << "Not Prime Number";
+
+    return 0;
+}
