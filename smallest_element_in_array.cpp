7bfeef07b869416ea3c72154420efa77c9a1ff05
Kusuma.B.J <kusumabj040406@gmail.com>
2026-03-15 19:55:04 +0530

    Create smallest_element_in_array.cpp

---
+++ smallest_element_in_array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int smallest = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+
+    cout << "Smallest element = " << smallest;
+
+    return 0;
+}
